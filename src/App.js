18ef38794e51a037c7faeeed1f65c852c1a562f9
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-04-28 11:12:28 -0700

    finished rendering first list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import JobList from './JobList.js';
+import { jobs } from './Jobs-data.js';
 import './App.css';
+
 // import your arrays here
 
 function App() {
   return (
     <div className="App">
-        Render all your lists here. Pass the arrays as props.
+      <JobList jobs={jobs} />
     </div>
   );
 }
